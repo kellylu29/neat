805027e6ed921c713396d2891efe7ddb4877e200
Kelly <kellylovelu@gmail.com>
2017-09-24 21:31:09 -0400

    add scrollmagic

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,6 +3,25 @@
 // You can use CoffeeScript in this file: http://coffeescript.org/
 //= require_scrollmagic ./scrollmagic.js
 document.addEventListener("DOMContentLoaded", function(event) {
+  // init
+	var controller = new ScrollMagic.Controller();
+
+	// define movement of panels
+	var wipeAnimation = new TimelineMax()
+		.fromTo("section.panel.two", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})  // in from left
+		.fromTo("section.panel.three", 1, {x:  "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
+		.fromTo("section.panel.four", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone}); // in from top
+
+	// create scene to pin and link animation
+	new ScrollMagic.Scene({
+			triggerElement: "#pinContainer",
+			triggerHook: "onLeave",
+			duration: "300%"
+		})
+		.setPin("#pinContainer")
+		.setTween(wipeAnimation)
+		.addTo(controller);
+
   var buttons = document.querySelectorAll('.btn')
   var inputs = document.querySelectorAll('.input')
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
+
+  get '/users/:id', to: "users#show", as: "user"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
 end
