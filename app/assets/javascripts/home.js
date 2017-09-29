de9fceb5cd1d3d7b4bcd174c08c5dc2ba54a5c2c
Kelly <kellylovelu@gmail.com>
2017-09-28 23:04:04 -0400

    add typing effect and styling

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -37,6 +37,21 @@ window.onload = function () {
 	var selCuisine  = id('selectCuisine')
 	var endBtn 			= id('end_btn')
 
+	//Typing effect
+	var elements = document.getElementsByClassName('typewrite');
+	for (var i=0; i<elements.length; i++) {
+			var toRotate = elements[i].getAttribute('data-type');
+			var period = elements[i].getAttribute('data-period');
+			if (toRotate) {
+				new TxtType(elements[i], JSON.parse(toRotate), period);
+			}
+	}
+	// INJECT CSS for Typing Effect
+	var css = document.createElement("style");
+	css.type = "text/css";
+	css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff; margin: 10px;}";
+	document.body.appendChild(css);
+
 	// Event Listeners
   buttons.forEach(function(button) {
     button.addEventListener('click', function(e){
@@ -71,6 +86,7 @@ window.onload = function () {
   })
 
   endBtn.addEventListener('click', getRecipes)
+
 }
 
 function getRecipes(){
