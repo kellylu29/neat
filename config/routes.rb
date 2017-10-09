7b0b8dcdff148686b9bdd3c8a060c0a6a37415ba
Kelly <kellylovelu@gmail.com>
2017-10-08 20:59:50 -0400

    add omniauth-google

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :recipes
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   get '/users/:id', to: "users#show", as: "user"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
